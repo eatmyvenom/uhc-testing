16cc14e907492bc33e0ed2a266b3808652787009
eatmyvenom <eat.my.venomm@gmail.com>
2024-07-04 00:52:32 -0700

    adjust to be able to specify port

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-exec java -Xms4G -Xmx8G -jar paper-1.8.8-445.jar nogui
+
+DEFAULT_PORT=25565
+# Check if the file exists
+if [ -f "./PORT" ]; then
+    # File exists, read the file
+    PORT=$(cat "./PORT")
+else
+    # File does not exist, use the default value
+    PORT="$DEFAULT_PORT"
+fi
+
+exec java -Xms4G -Xmx8G -jar paper-1.8.8-445.jar -p $PORT
